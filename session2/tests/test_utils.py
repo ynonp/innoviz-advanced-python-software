c86219df060f7227129cc34ee1b564137cebb867
ynonp <ynonperek@gmail.com>
2020-12-28 11:02:51 +0200

    pytest demos

---
+++ session2/tests/test_utils.py
@@ -0,0 +1,9 @@
+import lib.utils as utils
+
+def test_square():
+    assert utils.square(2)  == 4
+    assert utils.square(3)  == 9
+    assert utils.square(-2) == 4
+
+def test_another_one():
+    assert 1 == 1
