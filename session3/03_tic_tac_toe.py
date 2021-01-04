cfa9eea9f866e3966377f5ecd4c9718a0968662b
ynonp <ynonperek@gmail.com>
2021-01-04 11:02:17 +0200

    ADD XO exercise

--- session3/03_tic_tac_toe.py
+++ session3/03_tic_tac_toe.py
@@ -8,17 +8,28 @@ board = np.array([
 
 # return the row <row_index>
 def row(board, row_index):
-    pass
+    return board[row_index, :]
+
 
 # return the column <col_index>
 def column(board, col_index):
-    pass
+    return board[:, col_index]
+
 
 # return the first diagonal (' ', 'O', ' ')
 def diag1(board):
-    pass
+    return board[[0, 1, 2], [0, 1, 2]]
+    #return np.diag(board)
 
 
 # return the second diagonal (' ', 'O', ' ')
 def diag2(board):
+    return board[[0, 1, 2], [2, 1, 0]]
+
+
+# Return True if somebody (X or O) won
+# Use boolean masks, np.all, slices
+def has_winner(board):
     pass
+
+
